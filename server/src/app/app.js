3b5c284f5829fc7e98732e6e9f993dfe375bed0e
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-01-19 16:38:26 +0100

    Add: AuthController added.

--- server/src/app/app.js
+++ server/src/app/app.js
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import { authRouter } from "../router/authRouter.js";
 
 dotenv.config();
 const app = express();
@@ -17,6 +18,9 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(cors());
+
+// Router
+app.use("/auth", authRouter);
 
 // Export
 export { app };
