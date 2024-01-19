e3e9f75cdc3a53d8852d30ca3e0e584a40810ccf
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-01-19 15:41:34 +0100

    Add: Middlewares configured.

--- server/src/app/app.js
+++ server/src/app/app.js
@@ -1,5 +1,16 @@
+// Imports
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
+// Middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cors());
+
+// Export
 export { app };
